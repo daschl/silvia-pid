e4fb4021c0a27cc7a44fc176df6eb6cdad6dde04
Michael Nitschinger <michael@nitschinger.at>
2020-04-14 18:32:52 +0200

    Run with misconfigured display

--- silvia-pid.ino
+++ silvia-pid.ino
@@ -1,9 +1,79 @@
+#include <Adafruit_GFX.h>
+#include <SPI.h>
+
+#include "Adafruit_SSD1351.h"
+#include "bitmaps.h"
+
+// ------------------------
+// --- Global Constants ---
+// ------------------------
+
+#define DEBUG             1
+#define SERIAL_BAUD  115200
+
+#define MAJOR_VERSION     0
+#define MINOR_VERSION     0
+#define PATCH_VERSION     1
+
+// ------------------------------
+// --- Display Initialization ---
+// ------------------------------
+
+#define DISPLAY_DC_PIN    4
+#define DISPLAY_CS_PIN    2
+#define DISPLAY_RST_PIN   5
+
+#define DISPLAY_WIDTH   128
+#define DISPLAY_HEIGHT  128
+
+Adafruit_SSD1351 display = Adafruit_SSD1351(
+  DISPLAY_WIDTH, 
+  DISPLAY_HEIGHT, 
+  &SPI, 
+  DISPLAY_CS_PIN, 
+  DISPLAY_DC_PIN, 
+  DISPLAY_RST_PIN
+);
+
+// -------------
+// --- Setup ---
+// -------------
+
 void setup() {
-  // put your setup code here, to run once:
+  logInit();
+  displayInit();
+  renderBootLogo();
+}
+
+// ---------------------
+// --- Main Run Loop ---
+// ---------------------
+
+void loop() {
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+// -------------------------
+// --- Display Functions ---
+// -------------------------
 
+// Initializes the display
+void displayInit() {
+  display.begin();
+}
+
+// Renders the boot logo with version
+void renderBootLogo() {
+  display.fillScreen(0xFFFF);
+  display.drawRGBBitmap(17,17, bootLogo, 94, 94);
+}
+
+// -----------------------
+// --- Debug Functions ---
+// -----------------------
+void logInit() {
+  #ifdef DEBUG
+  Serial.begin(SERIAL_BAUD);
+  Serial.println("Starting Siliva PID");
+  #endif
 }
